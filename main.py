bef9afd183524fd8216a5192f354ffe87c049abf
Resstaea <dwifatkhurohman4@gmail.com>
2026-06-07 16:33:15 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route('/crapi/had/viewstats', methods=['GET'])
+def viewstats():
+    token = request.args.get('token')
+    sEcho = request.args.get('sEcho', 1)
+    
+    # Token ambil dari Railway Variables biar aman
+    API_TOKEN = os.environ.get('API_TOKEN', 'hYuwoskkkaw28kssx==')
+    
+    if token != API_TOKEN:
+        return jsonify({"error": "Invalid token"}), 401
+
+    result = {
+        "sEcho": int(sEcho),
+        "iTotalRecords": 0,
+        "iTotalDisplayRecords": 0,
+        "aaData": []
+    }
+    
+    return jsonify(result)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
